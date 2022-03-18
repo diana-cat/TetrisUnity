ed9d9b7c426c068d3bf3c97ae15d6b4fb1ca54a6
Tetris <shdv.19@uni-dubna.ru>
2022-03-18 20:23:43 +0300

    moveAndRotate

--- Assets/Figure.cs
+++ Assets/Figure.cs
@@ -1,12 +1,16 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Figure : MonoBehaviour
-{   
+{
+    [SerializeField]
     private Vector3Int pivot;
     [SerializeField]
     private Vector3Int[] points; // 4 points
     [SerializeField]
     private TypeFigure type;
+    [SerializeField]
+    private Grid grid;
 
     private void Update()
     {
@@ -14,11 +18,34 @@ public class Figure : MonoBehaviour
         {
             RotateLeft();        
         }
+
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            MoveLeft();
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            MoveRight();
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            MoveDown();
+        }
+    }
+
+    private IEnumerable<Vector3Int> PointsPivot()
+    {
+        foreach (var item in points)
+        {
+            yield return item + pivot;
+        }
     }
 
     private void OnDrawGizmos()
     {
-        foreach (var item in points)
+        foreach (var item in PointsPivot())
         {
             Gizmos.DrawCube(item, Vector3.one);
         }
@@ -27,19 +54,31 @@ public class Figure : MonoBehaviour
     public void MoveLeft()
     {
         pivot += Vector3Int.left;
+        if (!grid.IsCellsEmpty(PointsPivot()))
+        {
+            pivot += Vector3Int.right;
+        }
     }
 
     public void MoveRight()
     {
         pivot += Vector3Int.right;
+        if (!grid.IsCellsEmpty(PointsPivot()))
+        {
+            pivot += Vector3Int.left;
+        }
     }
 
     public void MoveDown()
     {
         pivot += Vector3Int.down;
+        if (!grid.IsCellsEmpty(PointsPivot()))
+        {
+            pivot += Vector3Int.up;
+        }
     }
 
-    public void RotateLeft()
+    public void RotateLeftCount()
     {
         if (type == TypeFigure.Matrix3x3)
         {
@@ -53,6 +92,32 @@ public class Figure : MonoBehaviour
         else if (type == TypeFigure.Matrix4x4)
         {
             ChangePosition();
+        }
+    }
+
+    public void RotateRightCount()
+    {
+        if (type == TypeFigure.Matrix3x3)
+        {
+            for (int i = 0; i < points.Length; i++)
+            {
+                int x1 = points[i].x;
+                points[i].x = -points[i].y;
+                points[i].y = x1;
+            }
+        }
+        else if (type == TypeFigure.Matrix4x4)
+        {
+            ChangePosition();
+        }
+    }
+
+    public void RotateLeft()
+    {
+        RotateLeftCount();
+        if (!grid.IsCellsEmpty(PointsPivot()))
+        {
+            RotateRightCount();
         }
     }
 
--- Assets/Grid.cs
+++ Assets/Grid.cs
@@ -1,14 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
+[Serializable]
 public class Grid : MonoBehaviour
 {
-    private int width;
-    private int height;
+    private int width = 10;
+    private int height = 20;
     private int[] cells;
 
+    private void Start()
+    {
+        cells = new int[width * height];
+    }
 
+    public bool IsCellsEmpty(IEnumerable<Vector3Int> points)
+    {
+        foreach (var item in points)
+        {
+            if (Check(item))
+            {
+                return false;
+            }
+        }
+
+        return true;
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                Gizmos.DrawWireCube(new Vector3(i, j), Vector3.one * 0.9f);
+            }
+        }
+    }
+
+    private bool Check(Vector3Int point)
+    {
+        return (point.x < 0 || point.x >= width || point.y < 0 || point.y >= height);
+    }
 }
 
 enum TypeFigure
